b937b7ca47f0a94067d6199ef60a07cee24d39be
cskilian <csabakilian@fastmail.com>
2021-10-07 20:26:59 -0400

    Added functional mp3 player

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,8 @@
 //Element identifiers
 const AUDIO_PLAYER_ID = "audio-player";
 const PLAY_PAUSE_BUTTON = "play-pause-button";
+const TIMELINE_SEEKER = "seeker";
+const TIMELINE = "time-line";
 
 /*
  * Updates file label after upload
@@ -15,13 +17,21 @@ function selectFile(input, labelText)
  */
 function selectAudioFile(input, labelText)
 {
+	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
 	var fileReader = new FileReader();
 	fileReader.onload = (event) => {
-		const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
 		audioPlayer.src = event.target.result;
 	}
 	fileReader.readAsDataURL(input.files[0]);
+	//update UI
 	selectFile(input, labelText);
+	const seeker = document.getElementById(TIMELINE_SEEKER);
+	seeker.style.setProperty('background', 'black');
+	audioPlayer.addEventListener('canplaythrough', () => {
+		var duration = audioPlayer.duration;
+		audioPlayer.addEventListener('timeupdate', timeUpdate(duration), false);
+	}, false);
+
 }
 
 /*
@@ -59,3 +69,69 @@ function stopAudio(button)
 	}
 	audioPlayer.currentTime = 0;
 }
+
+/*
+ * Handler for when the audio has stopped playing
+ */
+function resetAudio()
+{
+	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+	const playPauseButton = document.getElementById(PLAY_PAUSE_BUTTON);
+	audioPlayer.currentTime = 0;
+	playPauseButton.innerText = "Play";
+}
+
+/*
+ * Returns an event handler that gets called by the audio-player to update the UI of the timeline
+ */
+ function timeUpdate(duration)
+ {
+ 	var timeUpdateFn = function() {
+ 		const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+ 		const seeker = document.getElementById(TIMELINE_SEEKER);
+ 		let currentPlace = 100 * (audioPlayer.currentTime / duration);
+ 		seeker.style.marginLeft = currentPlace + '%';
+ 	}
+ 	return timeUpdateFn;
+ }
+
+/* 
+ * Event handler for clicking on the timeline
+ */
+ function seekOnTimeline(event)
+ {
+ 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+ 	const seeker = document.getElementById(TIMELINE_SEEKER);
+ 	const timeline = document.getElementById(TIMELINE);
+ 	const timelineWidth = timeline.offsetWidth - seeker.offsetWidth;
+  	function moveSeeker(event)
+ 	{
+ 		const newMarginLeft = event.clientX - timeline.getBoundingClientRect().left;
+ 		if (0 <= newMarginLeft && newMarginLeft <= timelineWidth)
+ 		{
+ 			seeker.style.marginLeft = newMarginLeft + "px";
+ 		}
+ 		else if (newMarginLeft < 0)
+ 		{
+ 			seeker.style.marginLeft = "0px";
+ 		}
+ 		else
+ 		{
+ 			seeker.style.maginLeft = timelineWidth + "px";
+ 		}
+ 	}
+ 	if (audioPlayer.src == "")
+	{
+		return;
+	}
+ 	moveSeeker(event);
+ 	audioPlayer.currentTime = audioPlayer.duration * (event.clientX - timeline.getBoundingClientRect().left) / timelineWidth; 
+ }
+
+/*
+ * Initialize
+ */
+ function initAll()
+ {
+
+ }
