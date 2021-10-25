6b05b5b2e768cd347d58fcedf3ced804fef14656
cskilian <csabakilian@fastmail.com>
2021-10-25 19:17:10 -0400

    Refactored to separate concerns, fixed timeline UI bug

--- src/client/client.js
+++ src/client/client.js
@@ -1,32 +1,34 @@
 //Element identifiers
+const AUDIO_FILE_NAME = "audio-name";
+const SCORE_FILE_NAME = "score-name";
+const SYNC_FILE_NAME = "sync-name";
 const AUDIO_PLAYER_ID = "audio-player";
 const PLAY_PAUSE_BUTTON = "play-pause-button";
 const TIMELINE_SEEKER = "seeker";
 const TIMELINE = "time-line";
 
-/*
- * Updates file label after upload
- */
-function selectFile(input, labelText) 
-{
-    document.getElementById(labelText).innerHTML = input.files[0].name;
-}
 
-/*
- * Custom handler for selecting audio file. It sets the source for the player to the given audio file
+/* ====================================================================================
+ * Controllers
+ * ====================================================================================
  */
-function selectAudioFile(input, labelText)
+function stopAudioControl()
 {
 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+	audioPlayer.pause();
+	audioPlayer.currentTime = 0;
+}
+
+function changeAudioPlayerSource(file)
+{
+	stopAudioControl()
+	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+	const seeker = document.getElementById(TIMELINE_SEEKER);
 	var fileReader = new FileReader();
 	fileReader.onload = (event) => {
 		audioPlayer.src = event.target.result;
 	}
-	fileReader.readAsDataURL(input.files[0]);
-	//update UI
-	selectFile(input, labelText);
-	const seeker = document.getElementById(TIMELINE_SEEKER);
-	seeker.style.setProperty('background', 'black');
+	fileReader.readAsDataURL(file);
 	audioPlayer.addEventListener('canplaythrough', () => {
 		var duration = audioPlayer.duration;
 		audioPlayer.addEventListener('timeupdate', timeUpdate(duration), false);
@@ -34,10 +36,7 @@ function selectAudioFile(input, labelText)
 
 }
 
-/*
- * Handler for pressing play/pause button
- */
-function playPauseAudio(button)
+function playPauseAudioControl()
 {
 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
 	if (audioPlayer.src == "")
@@ -47,66 +46,47 @@ function playPauseAudio(button)
 	if (audioPlayer.paused)
 	{
 		audioPlayer.play();
-		button.innerText = "Pause";
 	}
 	else
 	{
 		audioPlayer.pause();
-		button.innerText = "Play";
 	}
 }
 
-/*
- * Handler for pressing stop button
- */
-function stopAudio(button)
+function changeAudioPlayerPosition(event)
 {
 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
-	const playPauseButton = document.getElementById(PLAY_PAUSE_BUTTON);
-	if (!audioPlayer.paused)
-	{
-		playPauseAudio(playPauseButton);
-	}
-	audioPlayer.currentTime = 0;
+ 	const timeline = document.getElementById(TIMELINE);
+ 	const timelineWidth = timeline.offsetWidth - seeker.offsetWidth;
+  	audioPlayer.currentTime = audioPlayer.duration * (event.clientX - timeline.getBoundingClientRect().left) / timelineWidth;
 }
 
-/*
- * Handler for when the audio has stopped playing
+/* =====================================================================================
+ * User Interface 
+ * =====================================================================================
  */
-function resetAudio()
+function updateLabel(labelId, fileName)
 {
-	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
-	const playPauseButton = document.getElementById(PLAY_PAUSE_BUTTON);
-	audioPlayer.currentTime = 0;
-	playPauseButton.innerText = "Play";
+	document.getElementById(labelId).innerHTML = fileName;
 }
 
+function updateSeeker()
+{
+	const seeker = document.getElementById(TIMELINE_SEEKER);
+	seeker.style.setProperty('background', 'black');
+}
 /*
  * Returns an event handler that gets called by the audio-player to update the UI of the timeline
  */
- function timeUpdate(duration)
- {
- 	var timeUpdateFn = function() {
+function timeUpdate(duration)
+{
+	var timeUpdateFn = function() {
  		const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
  		const seeker = document.getElementById(TIMELINE_SEEKER);
- 		let currentPlace = 100 * (audioPlayer.currentTime / duration);
- 		seeker.style.marginLeft = currentPlace + '%';
- 	}
- 	return timeUpdateFn;
- }
-
-/* 
- * Event handler for clicking on the timeline
- */
- function seekOnTimeline(event)
- {
- 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
- 	const seeker = document.getElementById(TIMELINE_SEEKER);
- 	const timeline = document.getElementById(TIMELINE);
- 	const timelineWidth = timeline.offsetWidth - seeker.offsetWidth;
-  	function moveSeeker(event)
- 	{
- 		const newMarginLeft = event.clientX - timeline.getBoundingClientRect().left;
+ 		const timeline = document.getElementById(TIMELINE);
+ 		const timelineWidth = timeline.offsetWidth - seeker.offsetWidth;
+ 		const currentPlace = audioPlayer.currentTime / duration;
+ 		const newMarginLeft = currentPlace * timelineWidth;
  		if (0 <= newMarginLeft && newMarginLeft <= timelineWidth)
  		{
  			seeker.style.marginLeft = newMarginLeft + "px";
@@ -120,18 +100,107 @@ function resetAudio()
  			seeker.style.maginLeft = timelineWidth + "px";
  		}
  	}
- 	if (audioPlayer.src == "")
+ 	return timeUpdateFn;
+}
+
+function updatePlayPauseButton()
+{
+	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+	const button = document.getElementById(PLAY_PAUSE_BUTTON);
+	if (audioPlayer.src == "" || audioPlayer.paused)
+	{
+		button.innerText = "Play";
+	}
+	else
+	{
+		button.innerText = "Pause";
+	}
+
+}
+
+function moveSeeker(event)
+{
+ 	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+ 	const seeker = document.getElementById(TIMELINE_SEEKER);
+ 	const timeline = document.getElementById(TIMELINE);
+ 	const timelineWidth = timeline.offsetWidth - seeker.offsetWidth;
+ 	const newMarginLeft = event.clientX - timeline.getBoundingClientRect().left;
+ 	if (0 <= newMarginLeft && newMarginLeft <= timelineWidth)
+ 	{
+ 		seeker.style.marginLeft = newMarginLeft + "px";
+ 	}
+ 	else if (newMarginLeft < 0)
+ 	{
+ 		seeker.style.marginLeft = "0px";
+ 	}
+ 	else
+ 	{
+ 		seeker.style.maginLeft = timelineWidth + "px";
+ 	}
+}
+/* =======================================================================================
+ * Event Handlers
+ * =======================================================================================
+ */
+function selectSyncFile(input)
+{
+	const fileName = input.files[0].name;
+	updateLabel(SYNC_FILE_NAME, fileName);
+}
+/*
+ * Updates file label after upload
+ */
+function selectScoreFile(input) 
+{
+	const fileName = input.files[0].name;
+	updateLabel(SCORE_FILE_NAME, fileName);
+}
+
+/*
+ * Custom handler for selecting audio file. It sets the source for the player to the given audio file
+ */
+function selectAudioFile(input)
+{
+	changeAudioPlayerSource(input.files[0]);
+	updateLabel(AUDIO_FILE_NAME, input.files[0].name);
+	updateSeeker();
+	updatePlayPauseButton();
+}
+
+
+function playPauseAudio()
+{
+	playPauseAudioControl();
+	updatePlayPauseButton();
+}
+
+function stopAudio()
+{
+	stopAudioControl();
+	updatePlayPauseButton();
+}
+
+function resetAudio()
+{
+	stopAudioControl();
+	updatePlayPauseButton();
+}
+
+
+/* 
+ * Event handler for clicking on the timeline
+ */
+function seekOnTimeline(event)
+{
+	const audioPlayer = document.getElementById(AUDIO_PLAYER_ID);
+	if (audioPlayer.src == "")
 	{
 		return;
 	}
- 	moveSeeker(event);
- 	audioPlayer.currentTime = audioPlayer.duration * (event.clientX - timeline.getBoundingClientRect().left) / timelineWidth; 
- }
+	changeAudioPlayerPosition(event);
+	moveSeeker(event);
+}
 
-/*
- * Initialize
- */
  function initAll()
  {
-
  }
