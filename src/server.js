8aa872b86be79e20853f94a3ddd4c89a24c09349
cskilian <csabakilian@fastmail.com>
2021-11-06 01:27:25 -0400

    Fixed paths to use client dir variable

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const CLIENT_DIR = path.join(__dirname, 'client');
+const CLIENT_DIR = 'client';
 
-app.use('/css', express.static(path.join(__dirname, 'client/css')));
-app.use('/', express.static(CLIENT_DIR));
+app.use('/css', express.static(path.join(__dirname, `${CLIENT_DIR}/css`)));
+app.use('/', express.static(path.join(__dirname, CLIENT_DIR)));
 
 app.get('/', (req, res) => {
 	res.sendFile('client.html', { root: CLIENT_DIR});
